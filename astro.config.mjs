6bb342f30649e18702fea1f5a58b536397146077
Son Chau <soncqb@gmail.com>
2024-01-21 07:59:57 -0800

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://incandescent-nasturtium-e951cd.netlify.app",
+  integrations: [preact()]
+});
